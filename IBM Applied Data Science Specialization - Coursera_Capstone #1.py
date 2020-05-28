ab8c65499d1b34473e677d37070c34f55702c7d2
ZNaddie <62944562+ZNaddie@users.noreply.github.com>
2020-05-28 03:39:42 +0300

    Add files via upload
    Create a new repository on your Github account and name it Coursera_Capstone. I have prepared a guide to walk you through the process of creating a repository and setting it up. For Mac users, click here. For Windows users, click here.
    
    Now, start a Jupyter Notebook using any platform that you are comfortable with and do the following:
    
    Write some markdown to explain that this notebook will be mainly used for the capstone project.
    Import the pandas library as pd.
    Import the Numpy library as np.
    Print the following the statement: Hello Capstone Project Course!
    Push the Notebook to your Github repository and submit a link to the notebook on your Github repository.

---
+++ IBM Applied Data Science Specialization - Coursera_Capstone #1.py
@@ -0,0 +1,10 @@
+# this notebook will be mainly used for the \
+# IBM Applied Data Science Capstone project
+
+# Source: https://www.coursera.org/learn/applied-data-science-capstone
+
+import pandas as pd
+
+import numpy as np
+
+print("Hello Capstone Project Course")
